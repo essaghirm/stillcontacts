a42d89159a17e79db026e61af827e742fdcbfcf8
Mouhcine <essaghir.dev@gmail.com>
2018-11-05 02:47:52 +0100

    diagnostic

--- src/pages/diagnostic/diagnostic.ts
+++ src/pages/diagnostic/diagnostic.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, Loading } from 'ionic-angular';
 import { Http } from '@angular/http';
+import { DetailPage } from '../detail/detail';
 
 /**
  * Generated class for the DiagnosticPage page.
@@ -18,22 +19,48 @@ export class DiagnosticPage {
   type:any
   contacts:any = []
   url = "http://localhost:8000"
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+  loading: any
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DiagnosticPage');
   }
   onChange(){
+    this.presentLoading()
     this.http.get(this.url+'/diagnostic/'+this.type).map(res => res.json()).subscribe(
       data => {
           console.log('onChange: ', data)
           this.contacts = data
+          this.loadingDismiss()
       },
       err => {
           console.log("Oops!")
       }
   )
   }
+
+  details(contact) {
+		// this.setMostViewed(contact)
+		this.navCtrl.push(DetailPage, {
+			contact: contact
+		})
+  }
+  
+  presentLoading() {
+    this.loading = this.loadingCtrl.create({
+        content: 'Please wait...'
+    });
+
+    this.loading.present();
+
+    setTimeout(() => {
+        this.loading.dismiss();
+    }, 300000);
+}
+
+loadingDismiss() {
+    this.loading.dismiss()
+}
 
 }
